4d146a028317a636bb2a8abf931f86deed9fcdc2
languyenhammel <cryphum@gmail.com>
2026-03-13 12:06:33 -0400

    LSU finished (not tested)

--- rtl2/module loadstore_unit.sv
+++ rtl2/module loadstore_unit.sv
@@ -1,9 +1,11 @@
+import CORE_PKG::*;
+
 module loadstore_unit (
     input logic     clk,
     input logic     rst_n,
     input logic     en_lsu,
 
-    input logic [3:0] lsu_operator,
+    input load_store_func_code lsu_operator_op, // which LSU instruction to do 
     input  logic [31:0] alu_result, // memory address
 
     // Store instruction (info to store)
@@ -11,26 +13,16 @@ module loadstore_unit (
     output logic [31:0] mem_wdata,  // same data to write to memory, just repackaged for the output
     output logic mem_wen, // a bit that goes high when we're trying to write to memory (store instruction)
 
-    output logic [3:0] mem_be, // a register that specifies which byte to write to for store byte 
+    output logic [3:0] mem_be, // a register that specifies which byte to write to (for store byte only?)
 
     output logic [31:0] mem_addr, // address sent to memory to retrieve the actual (data) input to LSU 
-    input logic [31:0] input_data, // data coming back from the memory 
+    input logic [31:0] input_data, // data coming back from the memory (for load instructions)
 
     // To writeback, for load instructions (figure out specifics later)
-    out logic [31:0] writeback_shit;
+    output logic [31:0] writeback_output;
 );
 
-
-    typedef enum logic [2:0] {
-        FETCH,
-        EXECUTE,
-        MEMORY, 
-        STAGE // add mroe STAGES later
-    } state_t;
-    state_t state;
-
     logic [31:0] instruction; // internal register for storing the instruction
-    logic [31:0] pc;
 
     // ======================= Instruction decode section START =======================
     // The assignment operations are wires outside of the flip-flop section to ensure continuous updates
@@ -47,24 +39,43 @@ module loadstore_unit (
     wire [4:0] s_rs2 = instruction[24:20];
     wire [11:0] s_imm = {instruction[31:25], instruction[11:7]};
 
+    // U-type instructions
+    wire [4:0] u_rd = instruction[11:7];
+    wire [20:0] u_imm = instruction[31:12];
+
     // ======================= Instruction decode section END =======================
 
     always_comb begin 
         mem_wen = 1'b0;
-        mem_be = 4'b0000;
-        writeback_shit = 32'b0;
+        mem_be = 4'b1111;
+        writeback_output = 32'b0;
+        mem_addr = alu_result;
+        mem_wdata = 32'b0; 
 
         if(en_lsu) begin
-            case(lsu_operator)
-                LW: 
-                SW:
-                LB:
-                SB
-
+            case(lsu_operator_op)
+                LW: begin // Load word
+                    writeback_output = input_data;
+                end
+                LB: begin // Load byte
+                    writeback_output = {{24{input_data[7]}}, input_data[7:0]};
+                end
+                SW: begin // Store word
+                    mem_wen = 1'b1;
+                    mem_wdata = regfile_b_out;
+                end
+                SB: begin // Store byte
+                    mem_wen = 1'b1;
+                    mem_be = 4'b0011;
+                    case(alu_result[1:0]) 
+                        2'b00: mem_wdata = {24'b0, mem_wdata[7:0]};
+                        2'b01: mem_wdata = {16'b0, mem_wdata[7:0], 8'b0};
+                        2'b10: mem_wdata = {8'b0, mem_wdata[7:0], 16'b0};
+                        2'b11: mem_wdata = {mem_wdata[7:0], 24'b0};
+                    endcase
+                end
             endcase
         end
-
-
     end
 endmodule
     
