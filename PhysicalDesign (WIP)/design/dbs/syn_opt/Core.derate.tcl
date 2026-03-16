ebb491a7cdfe173956da64bd89b1b33e39b6441a
Khalil <dkhalil8@ece-rschsrv.ece.gatech.edu>
2026-03-15 21:44:32 -0400

    unlink

--- PhysicalDesign (WIP)/design/dbs/syn_opt/Core.derate.tcl
+++
@@ -1,2 +0,0 @@
-set_timing_derate -delay_corner tt_v1.8_25C_Nominal_25 -cell_delay 1  
-set_timing_derate -delay_corner tt_v1.8_25C_Nominal_25 -net_delay 1  
--- PhysicalDesign (WIP)/design/dbs/syn_opt/Core.dont_touch.sdc
+++
@@ -1,2 +0,0 @@
-set_dont_use [get_lib_cells ICGX1]
-set_dont_use [get_lib_cells sky130_sram_2kbyte_1rw1r_32x512_8]
